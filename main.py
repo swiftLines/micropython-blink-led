c19e922af284ac56e00b45d9a10253b7d23f7a2f
Jeremy Underwood <underwood.j248@gmail.com>
2025-08-19 10:29:33 -0400

    change led blink speed

--- main.py
+++ main.py
@@ -3,9 +3,26 @@ import utime
 
 led = machine.Pin("LED", machine.Pin.OUT)
 
+FAST = 0.1  # seconds
+SLOW = 0.5
+
+delay = SLOW
+
 while True:
     led.value(1)
-    utime.sleep(1.5)
+    utime.sleep(delay)
     led.value(0)
-    utime.sleep(1.5)
+    utime.sleep(delay)
+    
+# 1
+# import machine
+# import utime
+
+# led = machine.Pin("LED", machine.Pin.OUT)
+
+# while True:
+#     led.value(1)
+#     utime.sleep(1.5)
+#     led.value(0)
+#     utime.sleep(1.5)
     
