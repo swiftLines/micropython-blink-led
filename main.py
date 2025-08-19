2f222b640e54aa14158af9f3f76ce78409456640
Jeremy Underwood <underwood.j248@gmail.com>
2025-08-19 10:41:45 -0400

    double-blink pattern

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
+# 3 Double-blink pattern
 import machine
 import utime
 
 led = machine.Pin("LED", machine.Pin.OUT)
 
-FAST = 0.1  # seconds
-SLOW = 0.5
-
-delay = SLOW
-
+def pulse(on_ms, off_ms, count):
+    for _ in range(count):
+        led.value(1)
+        utime.sleep_ms(on_ms)
+        led.value(0)
+        utime.sleep_ms(off_ms)
 while True:
-    led.value(1)
-    utime.sleep(delay)
-    led.value(0)
-    utime.sleep(delay)
-    
-# 1
+    # two quick blinks
+    pulse(100, 100, 2)
+    utime.sleep(1.0)
+
+
+# 2 Change Blink Speed
+# import machine
+# import utime
+
+# led = machine.Pin("LED", machine.Pin.OUT)
+
+# FAST = 0.1  # seconds
+# SLOW = 0.5
+
+# delay = SLOW
+
+# while True:
+#     led.value(1)
+#     utime.sleep(delay)
+#     led.value(0)
+#     utime.sleep(delay)
+
+
+# 1 Blink LED
 # import machine
 # import utime
 
